ed62587a9a04d701dc86921f3595d653ec5e4995
Nisan <Nisan@DESKTOP-G2J4OA2>
2023-12-23 00:01:24 +0200

    Add project files.

---
+++ TinyMachine/TinyMachine.cpp
@@ -0,0 +1,12 @@
+﻿// TinyMachine.cpp : Defines the entry point for the application.
+//
+
+#include "TinyMachine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
